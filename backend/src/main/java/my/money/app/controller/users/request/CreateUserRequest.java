26f087f30de9f4a93fd1c5355aec6a26af2f532a
Matheus-Soares-Duarte <matheussoares_09@hotmail.com>
2021-06-14 18:21:15 -0300

    UserServiceImplTest, validação de CPF implementada

--- backend/src/main/java/my/money/app/controller/users/request/CreateUserRequest.java
+++ backend/src/main/java/my/money/app/controller/users/request/CreateUserRequest.java
@@ -1,12 +1,10 @@
 package my.money.app.controller.users.request;
 
-import lombok.Builder;
 import lombok.Data;
 
 import java.time.LocalDate;
 
 @Data
-@Builder
 public class CreateUserRequest {
     String cpf;
     String username;
--- backend/src/main/java/my/money/app/controller/users/request/LoginRequest.java
+++ backend/src/main/java/my/money/app/controller/users/request/LoginRequest.java
@@ -1,10 +1,8 @@
 package my.money.app.controller.users.request;
 
-import lombok.Builder;
 import lombok.Data;
 
 @Data
-@Builder
 public class LoginRequest {
     String username;
     String password;
